b563f26d5867f23658b086fb2490c2b671d06a81
witz <wtzcrst196@gmail.com>
2026-03-10 16:14:07 -0400

    Update song.js

--- song.js
+++ song.js
@@ -1,26 +1,6 @@
-let songs = [
-    {
-        name:'7th Floor Tango - Silent Partner',
-        path:'music/song1.mp3',
-        artist:'Artist Y01',
-        image:'images/7th Floor Tango - Silent Partner.jpg'
-    },
-    {
-        name:'Arp Bounce - Geographer',
-        path:'music/song2.mp3',
-        artist:'Artist Y01',
-        image:'images/Arp Bounce - Geographer.jpg'
-    },
-    {
-        name:'BLAAM - Gunnar Olsen HipHop',
-        path:'music/song3.mp3',
-        artist:'Artist Y01',
-        image:'images/BLAAM - Gunnar Olsen HipHop.jpg'
-    },
-    {
-        name:'Hooky with Sloane - Bird Creek',
-        path:'music/song4.mp3',
-        artist:'Artist Y01',
-        image:'images/Hooky with Sloane - Bird Creek.jpg'
-    },
-];
+const songs=[
+{name:"Song 1",src:"music/song1.mp3"},
+{name:"Song 2",src:"music/song2.mp3"},
+{name:"Song 3",src:"music/song3.mp3"},
+{name:"Next door",src:"music/song4.mp3"}
+]
