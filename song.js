6762e3e5c67b2651e7d87cfa50fbfa15fe60fab7
witz <wtzcrst196@gmail.com>
2026-03-10 16:49:14 -0400

    Update song.js

--- song.js
+++ song.js
@@ -1,5 +1,5 @@
 const songs=[
-{name:"Song 1",src:"music/song1.mp3"},
+{name:"Song 1",src:"music/song1.mp3",artist:'Artist Y01'},
 {name:"Song 2",src:"music/song2.mp3"},
 {name:"Song 3",src:"music/song3.mp3"},
 {name:"Next door",src:"music/song4.mp3"}
