2688b417bb6cd35202e1060cb63b878777e2cf60
witz <wtzcrst196@gmail.com>
2026-03-10 16:05:29 -0400

    Create song.js

---
+++ song.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name:'7th Floor Tango - Silent Partner',
+        path:'music/song1.mp3',
+        artist:'Artist Y01',
+        image:'images/7th Floor Tango - Silent Partner.jpg'
+    },
+    {
+        name:'Arp Bounce - Geographer',
+        path:'music/song2.mp3',
+        artist:'Artist Y01',
+        image:'images/Arp Bounce - Geographer.jpg'
+    },
+    {
+        name:'BLAAM - Gunnar Olsen HipHop',
+        path:'music/song3.mp3',
+        artist:'Artist Y01',
+        image:'images/BLAAM - Gunnar Olsen HipHop.jpg'
+    },
+    {
+        name:'Hooky with Sloane - Bird Creek',
+        path:'music/song4.mp3',
+        artist:'Artist Y01',
+        image:'images/Hooky with Sloane - Bird Creek.jpg'
+    },
+];
