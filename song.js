78b9044b2a4254c12e39c72cb0debc8cc0fb964a
witz <wtzcrst196@gmail.com>
2026-03-10 16:54:18 -0400

    Update song.js

--- song.js
+++ song.js
@@ -1,6 +1,6 @@
 const songs=[
-{name:"Peaches",src:"music/song1.mp3",artist:'Artist Y01'},
-{name:"Song 2",src:"music/song2.mp3"},
-{name:"Song 3",src:"music/song3.mp3"},
-{name:"Next door",src:"music/song4.mp3"}
+{name:"Peaches",src:"music/song1.mp3",artist:'Justin Bieber'},
+{name:"Consume",src:"music/song2.mp3",artist:'Chase Atlantic'},
+{name:"Intention",src:"music/song3.mp3",artist:'Justin Bieber & Quavo'},
+{name:"Next door",src:"music/song4.mp3",artist:'ASTN & Amelia Moore'}
 ]
