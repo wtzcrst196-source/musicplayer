6e028cc8e5f4c4b43b44a2a6588a70549eb4724d
witz <wtzcrst196@gmail.com>
2026-03-10 16:40:43 -0400

    Update song.js

--- song.js
+++ song.js
@@ -1,6 +1,26 @@
 const songs=[
-{name:"Song 1",src:"music/song1.mp3"},
-{name:"Song 2",src:"music/song2.mp3"},
-{name:"Song 3",src:"music/song3.mp3"},
-{name:"Next door",src:"music/song4.mp3"}
+{
+name:"Song 1",
+artist:"Unknown",
+image:"images/photo-1.jpg",
+src:"music/song1.mp3"
+},
+{
+name:"Song 2",
+artist:"Unknown",
+image:"images/photo-1.jpg",
+src:"music/song2.mp3"
+},
+{
+name:"Song 3",
+artist:"Unknown",
+image:"images/photo-1.jpg",
+src:"music/song3.mp3"
+},
+{
+name:"Next door",
+artist:"Unknown",
+image:"images/photo-1.jpg",
+src:"music/song4.mp3"
+}
 ]
